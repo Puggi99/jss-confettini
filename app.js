c66ee03db3b568ccfc5dd3a36f731dde24849ac0
Puggi99 <127298032+Puggi99@users.noreply.github.com>
2023-05-02 20:03:42 +0200

    c

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
-https://www.kirilv.com/canvas-confetti/
+console.log('benvenuti in confettini!')
 
 function firstTest(){
     confetti({
-        particleCount: 400,
-        spread: 100,
-        origin: { y: 0.6 }
+        particleCount: 100,
+        spread: 360,
+        origin: { y: 0.5 },
+        shapes:['star'],
+        colors:['#d4af37'],
+        scalar: 0.3
       });
-
-    
 }
 
-const pippo = document.getElementById('pippo')
-const p = document.createElement('p')
-const text = document.createTextNode('ciao')
+const pippo = document.getElementById('pippo');
+
+const p = document.createElement('p');
+
+const text = document.createTextNode('ciao');
 
 p.appendChild(text);
+
 pippo.appendChild(p);
 
+
 $('#pippo').append($('<p>').text('ciao jquery'));
-const pluto = document.querySelector('.pluto')
 
-//versione JavaScript come faceva prima di copiare da jquery
-const plutoCollection = document.getElementsByClassName('pluto')
-const firstPluto = plutoCollection[0]
-console.log(firstPluto)
 
-//versione jquery
-const pluto2 = $('.pluto')[0]
-console.log(pluto2)
+const plutoCollection = document.getElementsByClassName('pluto');
+
+const firstPluto =  plutoCollection[0];
+
+console.log(firstPluto);
+
+
+const pluto2 = $('.pluto')[0];
+
+console.log(pluto2);
